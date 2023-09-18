9155623157b5a3bf36d7d6bd869e195a5a9ce70b
Yvan Douenel <y.douenel@coopernet.fr>
2023-09-18 16:15:28 +0000

    Ajout du theme custom theme_books.

---
+++ web/themes/custom/theme_books/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.theme_books = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
