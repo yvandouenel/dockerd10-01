458ad10437ded0f63b56bfc8a4ba47ccf9b8359a
Yvan Douenel <y.douenel@coopernet.fr>
2023-09-19 13:51:57 +0000

    module library_books avec injection de dependance EntityTypeManagerInterface ok.

--- web/modules/library_books/src/Controller/LibraryBooksManagement.php
+++ web/modules/library_books/src/Controller/LibraryBooksManagement.php
@@ -1,21 +1,62 @@
-<?php 
-namespace Drupal\library_books\Controller;
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\user\Entity\User;
+<?php
 
+namespace Drupal\library_books\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines a controller to load books management page.
+ */
 class LibraryBooksManagement extends ControllerBase {
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+        $container->get('entity_type.manager')
+      );
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * Méthode qui permet d'afficher la liste des livres.
+   */
   public function index() {
-    // Récupération des données de l'utilisateur
-    
-    $current_user = \Drupal::currentUser();
-    //dpm( $current_user);
-   
-    $drupal_account = User::load($current_user->id());
+    $query = $this->entityTypeManager->getStorage('library_books')->getQuery();
+    $query->accessCheck(TRUE);
+
+    $entities_id = $query
+      ->condition('bundle', 'book', '=')
+      ->condition('status', 1)
+      ->execute();
+    // $entities_id = [1, 2, 3];
+    // $drupal_account = User::load($this->current_user->id());
     return [
       '#theme' => 'books_management',
       '#user' => [
-        "name" => $current_user->getAccountName(),
+    // $current_user->getAccountName()
+        "name" => "Jojo",
       ],
+      "#books" => $entities_id,
     ];
   }
+
 }
